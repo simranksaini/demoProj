977588dabeb505f0b24a80d033723811d7fc95b7
Gursimran Kaur Saini <sainigursimrankaur@gmail.com>
2021-03-08 13:37:44 +0530

    'initial'

--- eureka-client1/eureka-client1/src/main/java/com/springboot/eurekaclient1/controller/clientController.java
+++ eureka-client1/eureka-client1/src/main/java/com/springboot/eurekaclient1/controller/clientController.java
@@ -6,15 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/client")
+//@RequestMapping("/")
 public class clientController {
 
     @Value("${config.server.key}")
     private String key1;
 
-    @GetMapping
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Cloud Demo Project";
+    }
+
+    @RequestMapping("/client")
     public String client() {
         return key1;
     }
+
 
 }
